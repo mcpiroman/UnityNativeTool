9fb50fd0542353aa5dc79884582b5e264ca1cd5d
MCpiroman <MCpiroman@gmail.com>
2019-02-02 17:29:30 +0100

    Added support for MarshalAsAttribute, OutAttribute and InAttribute

--- src/NativeFunction.cs
+++ src/NativeFunction.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
 
 namespace DllManipulator.Internal
 {
@@ -48,53 +44,6 @@ namespace DllManipulator.Internal
         {
             this.identity = identity;
             this.containingDll = containingDll;
-        }
-    }
-
-    internal class NativeFunctionSignature
-    {
-        public readonly Type returnParameterType;    
-        public readonly Type[] parameterTypes;
-        public readonly CallingConvention callingConvention;
-        public readonly bool bestFitMapping;
-        public readonly CharSet charSet;
-        public readonly bool setLastError;
-        public readonly bool throwOnUnmappableChar;
-
-        public NativeFunctionSignature(MethodInfo methodInfo, CallingConvention callingConvention, bool bestFitMapping, CharSet charSet, bool setLastError, bool throwOnUnmappableChar)
-        {
-            this.returnParameterType = methodInfo.ReturnType;
-            this.parameterTypes = methodInfo.GetParameters().Select(p => p.ParameterType).ToArray();
-            this.callingConvention = callingConvention;
-            this.bestFitMapping = bestFitMapping;
-            this.charSet = charSet;
-            this.setLastError = setLastError;
-            this.throwOnUnmappableChar = throwOnUnmappableChar;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is NativeFunctionSignature other &&
-                   EqualityComparer<Type>.Default.Equals(returnParameterType, other.returnParameterType) &&
-                   EqualityComparer<Type[]>.Default.Equals(parameterTypes, other.parameterTypes) &&
-                   callingConvention == other.callingConvention &&
-                   bestFitMapping == other.bestFitMapping &&
-                   charSet == other.charSet &&
-                   setLastError == other.setLastError &&
-                   throwOnUnmappableChar == other.throwOnUnmappableChar;
-        }
-
-        public override int GetHashCode()
-        {
-            var hashCode = 763644728;
-            hashCode = hashCode * -1521134295 + EqualityComparer<Type>.Default.GetHashCode(returnParameterType);
-            hashCode = hashCode * -1521134295 + EqualityComparer<Type[]>.Default.GetHashCode(parameterTypes);
-            hashCode = hashCode * -1521134295 + callingConvention.GetHashCode();
-            hashCode = hashCode * -1521134295 + bestFitMapping.GetHashCode();
-            hashCode = hashCode * -1521134295 + charSet.GetHashCode();
-            hashCode = hashCode * -1521134295 + setLastError.GetHashCode();
-            hashCode = hashCode * -1521134295 + throwOnUnmappableChar.GetHashCode();
-            return hashCode;
         }
     }
 }
